dd98c79e484f74579495a2195d4cd0e82faf4d19
Moearly <982221982@qq.com>
2015-05-06 18:26:45 +0800

    Initial commit

---
+++ app/src/main/java/com/weibo/martn/weiboapp/activity/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.weibo.martn.weiboapp.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Administrator on 2015/5/6.
+ */
+public class LoginActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
